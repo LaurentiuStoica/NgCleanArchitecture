9efd3d156cb5b119cd90b01c69c8a523d9631430
Laur <stoica363@gmail.com>
2025-01-15 12:55:54 +0200

    fixed test

---
+++ src/app/infrastructure/services/user-api.service.spec.ts
@@ -0,0 +1,37 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
+import { UserApiService } from './user-api.service';
+import { User } from '../../core/entities/user';
+
+describe('UserApiService', () => {
+  let service: UserApiService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        UserApiService,
+        { provide: HttpClient, useValue: httpClientSpy }, // Provide mocked HttpClient
+      ],
+    });
+
+    service = TestBed.inject(UserApiService);
+  });
+
+  it('should fetch users', () => {
+    const mockUsers: User[] = [
+      { id: 1, name: 'John Doe', email: 'john@example.com' },
+    ];
+
+    httpClientSpy.get.and.returnValue(of(mockUsers)); // Mock the HTTP GET request
+
+    service.fetchUsers().subscribe((users) => {
+      expect(users).toEqual(mockUsers);
+    });
+
+    expect(httpClientSpy.get).toHaveBeenCalledWith('https://jsonplaceholder.typicode.com/users');
+  });
+});
